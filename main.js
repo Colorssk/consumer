40e57499fb718eef13b4c2428fe1ccc54d279c13
colorssk <1043075409@qq.com>
2022-07-13 23:59:31 +0800

    init electron

---
+++ main.js
@@ -0,0 +1,16 @@
+const {app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+let mainwindow;
+
+app.on('ready', ()=>{
+    mainwindow = new BrowserWindow({
+        width: 1024,
+        height: 680,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    const urlLocation = isDev ? 'http://localhost:3000' : 'platformUrl'
+    mainwindow.loadURL(urlLocation)
+    mainwindow.webContents.openDevTools();
+});
